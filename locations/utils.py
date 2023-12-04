e6a72b37bac6ac940c76fdd1e0ece5b6308a7773
Jimbo Freedman <jimbo@huq.io>
2023-12-04 10:59:21 +0000

    fix: Date instead of DateTime, formatting

--- locations/utils.py
+++ locations/utils.py
@@ -6,12 +6,10 @@ from scrapy import Spider
 
 
 def feed_uri_params(params, spider: Spider):
-    job_data = json.loads(os.environ.get("JOB_DATA", "{}"))
-
     return {
         **params,
         "env": spider.settings.get("ENV"),
         "bucket": spider.settings.get("GCS_BUCKET"),
-        "schedule_date": os.environ.get("SCHEDULE_DATE"),
+        "schedule_date": spider.settings.get("SCHEDULE_DATE"),
         "spider_name": spider.name,
     }
